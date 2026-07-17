6d23479ddf7233a71a0fe16b3660d7d3057b3c89
arrag <arrag@example.com>
2026-07-18 02:00:30 +0530

    chore: commit production build assets to serve from Python backend

--- build.sh
+++ build.sh
@@ -2,13 +2,6 @@
 # exit on error
 set -o errexit
 
-# Build frontend
-echo "Building React frontend..."
-cd frontend_react
-npm install
-npm run build
-cd ..
-
 # Install backend dependencies
 echo "Installing backend dependencies..."
 pip install -r backend_api/requirements.txt
